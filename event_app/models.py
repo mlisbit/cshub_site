3c841e92ccb67c57ba201a061324ad6b674cea6f
bite <mlisbit@gmail.com>
2014-01-30 13:44:24 -0500

    html email sent on new event creation

--- event_app/models.py
+++ event_app/models.py
@@ -4,6 +4,11 @@ from time import time
 from datetime import datetime
 import django.utils.timezone
 
+#signals stuff:
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import EmailMultiAlternatives 
+
 def get_upload_file_name(instance, filename):
 	return "user_uploads/event_imgs/%s_%s" % (str(time()).replace('.','_'), filename)
 
@@ -31,7 +36,6 @@ class Comment(models.Model):
 	username = models.CharField(max_length=200)
 	body = models.TextField()
 	pub_date = models.DateTimeField('date published')
-
 	event = models.ForeignKey(Event)
 
 class Going(models.Model):	
@@ -41,3 +45,20 @@ class Going(models.Model):
 
 	def __unicode__(self): 
 		return self.username
+
+#send an email on creation of event. 
+@receiver(post_save, sender=Event)
+def send_email_on_event_creation(sender, **kwargs):
+	event_id 	= ''
+	event_name 	= ''
+	try:
+		event_id = Event.objects.get(name=kwargs.get('instance')).id
+		event_name = Event.objects.get(name=kwargs.get('instance')).name
+	except:
+		print 'Error, most likely due to duplicate Event name'
+
+	emails = list(User.objects.values_list('email', flat=True))
+	html_content="<b>York University Computing Students Hub</b><br>"+ event_name +": <a href='http://www.cshub.ca/events/get/"+str(event_id)+"'>Check it out</a>"
+	msg = EmailMultiAlternatives("New Event Posted on CSHUB", '', '', [emails])
+	msg.attach_alternative(html_content, "text/html")
+	msg.send()
