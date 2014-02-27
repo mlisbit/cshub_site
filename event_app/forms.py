7246047a923b343c789455b431077f305fed08a7
bite <mlisbit@gmail.com>
2014-02-27 00:53:08 -0500

    switched from bootflat to bootstrap 3.1, changed profile view, changed event listing view

--- event_app/forms.py
+++ event_app/forms.py
@@ -5,9 +5,7 @@ class CommentForm(forms.ModelForm):
 	class Meta:
 		model = Comment 
 		fields = ('body',)
-		widgets = {
-            'body': forms.TextInput(attrs={'class': 'comment_form'}),
-      }
+		widgets = {'body': forms.TextInput(attrs={'class': 'comment_form', 'placeholder': 'Type comment here...'}),}
 
 class GoingForm(forms.ModelForm):
 	class Meta:
--- userprofile/templatetags/profile_tags.py
+++ userprofile/templatetags/profile_tags.py
@@ -16,3 +16,17 @@ def number_going(user_positions):
 	for i in user_positions:
 		m.append(str(i))
 	return m
+
+@register.filter(name='has_social_account')
+def has_social_account(user):
+	u = User.objects.get(username=user).profile
+	if u.github_link or u.linkedin_link or u.twitter_link or u.facebook_link:
+		return True
+	return False
+
+@register.filter(name='has_badges')
+def has_badges(user):
+	u = User.objects.get(username=user).profile.club_position.all()
+	if u:
+		return True
+	return False
