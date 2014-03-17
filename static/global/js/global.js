83f668ed8a9e3ca9bd569dd063340ac6fde6fb75
bite <mlisbit@gmail.com>
2014-03-17 18:49:00 -0400

    notifications fixed

--- static/global/js/global.js
+++ static/global/js/global.js
@@ -11,26 +11,3 @@ $('.main-icon').click(function() {
 $('.carousel').carousel({
    interval: 3000
 });
-
-//banner image carousel touchs borders on small screens
-var border_touch = 730;
-//initial view
-if (window.innerWidth < border_touch ) {
-  if ($('#myCarousel').parent().attr("class") === 'container') {
-    $('#myCarousel').unwrap();
-  }
-}
-//on resize
-$(window).resize(function(){
-  console.log(window.innerWidth)
-  if (window.innerWidth < border_touch ) {
-    if ($('#myCarousel').parent().attr("class") === 'container') {
-      $('#myCarousel').unwrap();
-    }
-  }
-  else {
-    if ($('#myCarousel').parent().attr("class") !== 'container') {
-      $('#myCarousel').wrap('<div class="container"></div>');
-    }
-  }
-});
